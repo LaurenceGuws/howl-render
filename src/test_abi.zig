93324abb6c40a208ce40a5871834b691b579ca92
Laurence <lggousie46@gmail.com>
2026-06-01 13:36:35 +0200

    render: remove duplicate test entrypoints

--- src/test_abi.zig
+++
@@ -1,6 +0,0 @@
-const std = @import("std");
-
-test {
-    std.testing.refAllDecls(@import("libhowl_render.zig"));
-    _ = @import("test/ffi.zig");
-}
--- src/test_unit.zig
+++
@@ -1,3 +0,0 @@
-test {
-    _ = @import("test/unit/root.zig");
-}
