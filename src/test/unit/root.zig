f4a1a4950766d541d9bef4401964d5c55cdd8571
Laurence <lggousie46@gmail.com>
2026-06-06 12:27:32 +0200

    render: move realizer proofs to sibling test

--- src/test/unit/root.zig
+++ src/test/unit/root.zig
@@ -1,5 +1,5 @@
 test {
-    _ = @import("../../render/render_surface_realizer.zig");
+    _ = @import("../../render/render_surface_realizer_test.zig");
     _ = @import("../../prepared/render_surface_emitter_test.zig");
     _ = @import("../../prepared/owner_test.zig");
     _ = @import("../../render/geometry_test.zig");
