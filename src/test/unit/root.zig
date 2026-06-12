086eb9d8edca2f54869ae54f7e1ce5b072f45640
Laurence <lggousie46@gmail.com>
2026-06-12 20:44:10 +0200

    Rename prepared handle tests

--- src/test/unit/root.zig
+++ src/test/unit/root.zig
@@ -1,7 +1,7 @@
 test {
     _ = @import("../../geometry/render_surface_realizer_test.zig");
     _ = @import("../../prepared/render_surface_emitter_test.zig");
-    _ = @import("../../prepared/owner_test.zig");
+    _ = @import("../../prepared/handle_test.zig");
     _ = @import("../../geometry/geometry_test.zig");
     _ = @import("../../session/text_test.zig");
     _ = @import("../../text/ft_hb/support_test.zig");
