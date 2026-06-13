dbb866908787100a257c7cc937df0955285b8422
Laurence <lggousie46@gmail.com>
2026-06-13 16:59:42 +0200

    Delete render test wrapper

--- src/test_unit.zig
+++ src/test_unit.zig
@@ -1,3 +1,12 @@
 test {
-    _ = @import("test/unit/root.zig");
+    _ = @import("surface/realizer_test.zig");
+    _ = @import("surface/emitter_test.zig");
+    _ = @import("surface/handle_test.zig");
+    _ = @import("geometry_test.zig");
+    _ = @import("render_session.zig");
+    _ = @import("submitted_surface.zig");
+    _ = @import("c/text_session_test.zig");
+    _ = @import("c/submission_test.zig");
+    _ = @import("text/ft_hb/support_test.zig");
+    _ = @import("text/raster/special_test.zig");
 }
